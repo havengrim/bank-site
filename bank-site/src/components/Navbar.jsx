957239b1a3cfeb6d4a3b507253d925d1e48087de
Angelo Guiao <angeloguiao.gci@gmail.com>
2024-02-15 23:11:05 +0800

    discount

--- bank-site/src/components/Navbar.jsx
+++ bank-site/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         ))}
       </ul>
 
-      <div className="sm:hidden flex flex-1 justify-end items-center"> 
+      <div className="sm:hidden flex flex-1 justify-end items-center cursor-pointer "> 
           <img src={toggle ? close : menu }
             alt="menu"
             className="w-[28px] h-[28px] object-contain"
@@ -28,7 +28,7 @@ const Navbar = () => {
           <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
           <ul className="list-none flex flex-col justify-end flex-1">
             {navLinks.map((nav, index) =>(
-                <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px]   text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'}`}>
+                <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'}`}>
                   <a href={`#${nav.id}`}>
                     {nav.title}
                   </a>
